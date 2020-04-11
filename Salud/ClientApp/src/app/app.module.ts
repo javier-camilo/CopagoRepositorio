d4a3a1f59dcb6f3d00c9834fc72c6d48238b3032
javier-camilo <camiloviloriatovar@gmail.com>
2020-04-11 14:12:16 -0500

    formularios reactivos

--- Salud/ClientApp/src/app/app.module.ts
+++ Salud/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -34,6 +34,7 @@ import {MatListModule} from '@angular/material/list';
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
     MatMenuModule,
     MatIconModule,
--- Salud/ClientApp/src/app/copago/copago-registro/copago-registro.component.ts
+++ Salud/ClientApp/src/app/copago/copago-registro/copago-registro.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Copago } from '../models/copago';
 import { CopagoService } from '../../services/copago.service';
 import { NgForm } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-copago-registro',
@@ -10,23 +12,56 @@ import { NgForm } from '@angular/forms';
 })
 export class CopagoRegistroComponent implements OnInit {
 
+  formGroup: FormGroup;
   copago:Copago;
 
-  constructor(private copagoService: CopagoService) { }
+  constructor(private copagoService: CopagoService,  private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.copago = new Copago();
+    this. buildForm();
   }
 
+  private buildForm() {
 
-  add(formulario: NgForm){
+          this.copago.identificacionPaciente="";
+          this.copago.salarioTrabajador=0;
+          this.copago.valorServicio=0;
+
+          this.formGroup = this.formBuilder.group({ 
+
+      identificacionPaciente: [this.copago.identificacionPaciente, [Validators.required, Validators.maxLength(11)]],
+      valorServicio:  [this.copago.valorServicio,[Validators.required, Validators.min(1)]],
+      salarioTrabajador: [this.copago.salarioTrabajador,[Validators.required, Validators.min(1)]]
+
+
+    });
+  
+  }
+   
+
+  get control() { 
+    return this.formGroup.controls;
+  }
+
+  onSubmit() {
+  if (this.formGroup.invalid) {
+          return;
+        }
+        this.add();
+  }
+    
+    
+
+
+  add(){
+
+    this.copago=this.formGroup.value;
     this.copagoService.post(this.copago).subscribe(p => {
       if (p != null) {
         alert('registro correcto!');
       }
     });
-
-    this.clear(formulario);
 
   }
   
